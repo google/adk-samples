0bbaf2aefdb19bc3b96b4f489a49f6aabe56d6af
Berkay Güzel <98205992+berkayguzel06@users.noreply.github.com>
2025-05-03 00:51:13 +0300

    Update agents/readme-creator/eval/test_eval.py
    Co-authored-by: Jack Wotherspoon <jackwoth@google.com>

--- agents/readme-creator/eval/test_eval.py
+++ agents/readme-creator/eval/test_eval.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Basic evalualtion for Readme Creator."""
+"""Basic evaluation for Readme Creator."""
 
 import pathlib
 
